8682c8ec5724cca43586f50d2183e3a703e54d63
William Rich <williamrich@homerak.net>
2023-10-06 17:51:05 -0800

    fixed again

--- client/src/pages/dashboard/index.jsx
+++ client/src/pages/dashboard/index.jsx
@@ -3,18 +3,6 @@ import React from 'react';
 
 //Components
 
-const Dashboard = () => {
-    return (
-        <div>
-            <img />
-            <div>
-                <button>Dashboard</button>
-                <button>Messages</button>
-                <button>Calendar</button>
-            </div>
-            <button>Log Out</button>
-        </div>
-    );
-};
+const Dashboard = () => {};
 
 export { Dashboard };
