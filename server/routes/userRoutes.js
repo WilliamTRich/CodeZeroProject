cd3b60e55e6886fee2669238c38840fe0a391b54
Kate Anderson <kaanders00@gmail.com>
2023-10-08 16:38:09 -0400

    "removed front end sequelize & fixed db con file"

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,7 +2,7 @@ const Controller = require('../controllers/userController');
 
 module.exports = (app) => {
     app.get('/api', Controller.index)//test route
-    app.get('/api/trainer/:id', Controller.findOne)//find one
+    app.get('/api/user/:id', Controller.findOne)//find one
     app.get('/api/users', Controller.findAll)//find all
     app.post('/api/user/new', Controller.create)//create route
     app.put('/api/user/edit/:id', Controller.updateOne)//update one route
