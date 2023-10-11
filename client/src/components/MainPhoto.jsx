6a3152e70d9fc14f2000d1b91658b39c1049f547
whitchakara <whitneywilkerson@MacBook-Pro-4.local>
2023-10-10 21:03:59 -0500

    using tailwind css to center photo and inspiration

--- client/src/components/MainPhoto.jsx
+++ client/src/components/MainPhoto.jsx
@@ -2,16 +2,16 @@
 
 const MainPhoto = ()=> {
     return(     
-    <div className='mainphoto inline-flex'>
-        <img className="w-8/12" src="https://images.unsplash.com/photo-1483721310020-03333e577078?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3056&q=80" alt="" />
-        <div>
-        <p>
+    <div className='mainphoto flex items-center justify-center h-screen'>
+        <img className="w-8/12  ml-4" src="https://images.unsplash.com/photo-1483721310020-03333e577078?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3056&q=80" alt="" />
+        <div className="inspiration mx-5">
+        <p className="mb-4">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi alias eum eaque distinctio natus fuga culpa quos aliquid in neque cumque dicta, cum adipisci dignissimos sed commodi. Earum, debitis dicta.
         </p>
-        <p>
+        <p className="mb-4">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi alias eum eaque distinctio natus fuga culpa quos aliquid in neque cumque dicta, cum adipisci dignissimos sed commodi. Earum, debitis dicta.
         </p>
-        <p>
+        <p className="mb-4">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi alias eum eaque distinctio natus fuga culpa quos aliquid in neque cumque dicta, cum adipisci dignissimos sed commodi. Earum, debitis dicta.
         </p>
 
