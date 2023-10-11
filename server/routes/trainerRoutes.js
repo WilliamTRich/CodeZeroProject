081b3eb559fe28a05edc859e62274287faa37402
kander87 <kaanders00@gmail.com>
2023-10-10 21:38:00 -0400

    switched backend from mysql to mongodb, almost all backend files updated

--- server/routes/trainerRoutes.js
+++ server/routes/trainerRoutes.js
@@ -1,10 +1,16 @@
-const Controller = require('../controllers/trainerController');
+const trainerController = require('../controllers/trainerController');
+const { authenticate } = require('../config/jwtConfig'); // will remove in switch to oauth
+
 
 module.exports = (app) => {
-    app.get('/api', Controller.index)//test route
-    app.get('/api/trainer/:id', Controller.findOne)//find one
-    app.get('/api/trainers', Controller.findAll)//find all
-    app.post('/api/trainer/new', Controller.create)//create route
-    app.put('/api/trainer/edit/:id', Controller.updateOne)//update one route
-    app.delete('/api/trainer/:id', Controller.deleteOne)//delete one route
+    // app.get('/api/allTrainers', authenticate, trainerController.index)
+    // app.get('/api/cookie', trainerController.cookie)
+    app.post('/api/registerTrainer', trainerController.registerTrainer)
+    app.post('/api/loginTrainer', trainerController.loginTrainer)
+    app.get('/api/logoutTrainer', trainerController.logoutTrainer)
+    app.get('/api/getTrainer', trainerController.getTrainer)
+    app.get('/api/findUserAndTrainerById', trainerController.findUserAndTrainerById)
+    app.get('/api/allTrainers', trainerController.findAllTrainers)
+    app.get('/api/findOneTrainer', trainerController.findOneTrainer)
+
 }
