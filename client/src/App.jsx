c12c565bc544dbade071ab5bec03da32c1d8f0c9
whitchakara <whitneywilkerson@MacBook-Pro-4.local>
2023-10-31 16:45:57 -0500

    chanaged line 32 from navigate,localStorage to user,localStorage to fix refresh issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         console.log(e);
         navigate('/');
       });
-  }, [ navigate, localStorage]);
+  }, [ user, localStorage]);
 
   const routing = useRoutes(routes());
 
